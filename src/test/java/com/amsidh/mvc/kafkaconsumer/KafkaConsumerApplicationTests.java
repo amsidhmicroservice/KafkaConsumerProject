41a1915b4969b2b1a26e9b6f6cc358b46ae40f44
amsidh.lokhande@xebia.com <amsidh.lokhande@xebia.com>
2022-11-24 12:35:08 +0530

    Working with Springboot 2.0.6.RELEASE and spring-cloud-sleuth Finchley.SR3 version

--- src/test/java/com/amsidh/mvc/kafkaconsumer/KafkaConsumerApplicationTests.java
+++ src/test/java/com/amsidh/mvc/kafkaconsumer/KafkaConsumerApplicationTests.java
@@ -1,13 +1,13 @@
 package com.amsidh.mvc.kafkaconsumer;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KafkaConsumerApplicationTests {
+public class KafkaConsumerApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
